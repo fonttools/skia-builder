5ea153324793f9e2a5898fdee4c79895c32b5052
Cosimo Lupo <clupo@google.com>
2025-11-05 16:14:46 +0000

    disable legacy PNG encoding in image serialization
    Add SK_DISABLE_LEGACY_PNG_WRITEBUFFER preprocessor define to remove
    PNG encoder dependency from SkWriteBuffer serialization code. This
    allows Windows DLL builds to succeed without requiring PNG encoder
    implementation, as DLLs must resolve all symbols at link time.
    
    This matches Chromium's approach (see chromium/src@c97e564) and
    aligns with Skia's Bazel build which already disables this by default.

--- build_skia.py
+++ build_skia.py
@@ -44,6 +44,7 @@ SKIA_BUILD_ARGS = [
     "skia_enable_spirv_validation=false",
     "skia_use_lua=false",
     "skia_use_wuffs=false",
+    'extra_cflags=["-DSK_DISABLE_LEGACY_PNG_WRITEBUFFER"]',
 ]
 if sys.platform != "win32":
     # On Linux, I need this flag otherwise I get undefined symbol upon importing;
